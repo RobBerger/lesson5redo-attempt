f7ec136a268214716d36d7fbadc61b9a95637100
berger <robertberger.dev@gmail.com>
2023-02-23 15:16:59 -0500

    got local storage setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ContactActions from './ContactActions'
 import ContactForm from './ContactForm'
 import ContactList from './ContactList'
 import Stack from 'react-bootstrap/Stack'
 
 function App() {
+  const [contacts, setContacts] = useState([])
+  const [selectedContact, setSelectedContact] = useState(null)
+
+  function handleNewContact(contact) {
+    let newContactList = contacts.concat(contact)
+    setContacts(newContactList)
+    localStorage.setItem("contacts", JSON.stringify(newContactList));
+  }
+
+  function handleContactSelected(email) {
+    setSelectedContact(email)
+    localStorage.setItem("selectedContact", email)
+  }
+
+  function handleDeleteContact(email) {
+    setContacts(contacts.filter((contact) => contact.email !== email))
+    setSelectedContact(null)
+  }
+
   return (
     <Stack gap={3} className="col-md-10 mx-auto">
-      <ContactActions selectedContact={null} />
-      <ContactForm />
-      <ContactList contacts={[]} />
+      <ContactActions selectedContact={selectedContact} onDeleteContact={handleDeleteContact} />
+      <ContactForm onNewContact={handleNewContact} />
+      <ContactList contacts={contacts} onContactSelected={handleContactSelected} />
     </Stack>
   )
 }
--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
 function ContactForm(props) {
+  const [contact, setContact] = useState({
+    name: "", email: "", phone: ""
+  })
+
+  function handleChange(event) {
+    setContact({ ...contact, [event.target.name]: event.target.value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    props.onNewContact(contact)
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" >
         <Form.Label>Name</Form.Label>
-        <Form.Control type="text" name="name" />
+        <Form.Control type="text" value={contact.name} onChange={handleChange} name="name" />
       </Form.Group>
       <Form.Group className="mb-3" >
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" name="email" />
+        <Form.Control type="email" value={contact.email} onChange={handleChange} name="email" />
       </Form.Group>
       <Form.Group className="mb-3" >
         <Form.Label>Phone</Form.Label>
-        <Form.Control type="text" name="phone" />
+        <Form.Control type="text" value={contact.phone} onChange={handleChange} name="phone" />
       </Form.Group>
       <Button type="submit">Add Contact</Button>
     </Form>
