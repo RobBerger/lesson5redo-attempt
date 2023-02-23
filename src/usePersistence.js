c95843972811175a220f52e22a4d7c73758b4393
berger <robertberger.dev@gmail.com>
2023-02-23 15:45:58 -0500

    created usePersistence.js and added example code

---
+++ src/usePersistence.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+function usePersistence(key, defaultValue) {
+  const [value, setValue] = useState(()=> {
+    const persistedValue = localStorage.getItem(key);
+    if (persistedValue !== null) return JSON.parse(persistedValue)
+    return defaultValue;
+  })
+
+  useEffect(() => {
+    if (value === null) {
+      localStorage.removeItem(key)
+    } else {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  }, [value, key])
+
+  return [value, setValue]
+}
+
+export default usePersistence
