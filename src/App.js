aca5dfebbf3f4e9fac6b80873073abd094a3a981
berger <robertberger.dev@gmail.com>
2023-02-23 15:32:00 -0500

    updated the App.js file with new example code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ContactActions from './ContactActions'
 import ContactForm from './ContactForm'
 import ContactList from './ContactList'
@@ -8,24 +8,27 @@ function App() {
   const [contacts, setContacts] = useState([])
   const [selectedContact, setSelectedContact] = useState(null)
 
+  useEffect(() => {
+    setContacts(JSON.parse(localStorage.getItem("contacts") || "[]"))
+    setSelectedContact(localStorage.getItem("selectedContact"))
+  }, [])
+
   function handleNewContact(contact) {
-    let newContactList = contacts.concat(contact)
-    setContacts(newContactList)
-    localStorage.setItem("contacts", JSON.stringify(newContactList));
+    setContacts(contacts.concat(contact))
   }
 
   function handleContactSelected(email) {
     setSelectedContact(email)
-    localStorage.setItem("selectedContact", email)
   }
 
-  function handleDeleteContact(email) {
-    setContacts(contacts.filter((contact) => contact.email !== email))
+  function handleDeleteContact() {
+    setContacts(contacts.filter((contact) => contact.email !== selectedContact))
     setSelectedContact(null)
   }
 
   return (
     <Stack gap={3} className="col-md-10 mx-auto">
+    <br/>
       <ContactActions selectedContact={selectedContact} onDeleteContact={handleDeleteContact} />
       <ContactForm onNewContact={handleNewContact} />
       <ContactList contacts={contacts} onContactSelected={handleContactSelected} />
